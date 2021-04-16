da5e09f0b9abd85dc58f7ff4de86721eac4e89d8
Ashley Oliver <ashley.p.oliver@gmail.com>
2021-04-16 15:36:25 +0100

    end of tut 3 also bootstrapped base.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
+from polls import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
